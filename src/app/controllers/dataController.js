202823c738d4e0a41da09109d85e8f6b63762ec2
nhanbui1512 <buithiennhan0345@gmail.com>
2023-10-30 22:19:07 +0700

    get data from file csv then request to API

--- src/app/controllers/dataController.js
+++ src/app/controllers/dataController.js
@@ -1,7 +1,64 @@
-const sendData = (ws, req) => {
-  setInterval(() => {
-    ws.send("du lieu bang");
-  }, 1000);
+const axios = require('axios');
+
+async function sendRequestToServer({
+    ts,
+    idresp_p,
+    idorig_p,
+    orig_ip_bytes,
+    conn_state,
+    history,
+    orig_pkts,
+    proto,
+    duration,
+}) {
+    try {
+        const response = await axios.post(
+            'http://127.0.0.1:5000/predict',
+            {
+                ts: ts,
+                idresp_p: idresp_p,
+                idorig_p: idorig_p,
+                orig_ip_bytes: orig_ip_bytes,
+                conn_state: conn_state,
+                history: history,
+                orig_pkts: orig_pkts,
+                proto: proto,
+                duration: duration,
+            },
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data', // Đặt kiểu nội dung là form-data
+                },
+            },
+        );
+
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const sendData = (ws, data) => {
+    let i = 0;
+    setInterval(async () => {
+        try {
+            const res = await sendRequestToServer({
+                ts: data[i][1],
+                idresp_p: data[i][6],
+                idorig_p: data[i][4],
+                orig_ip_bytes: data[i][18],
+                orig_pkts: data[i][17],
+                conn_state: data[i][12],
+                history: data[i][16],
+                proto: data[i][7],
+                duration: data[i][9],
+            });
+            ws.send(JSON.stringify(res)); // send to client UI through websocket
+            i++;
+        } catch (error) {
+            console.log(error);
+        }
+    }, 2000);
 };
 
 module.exports = sendData;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
-const express = require("express");
-const Websocket = require("ws");
 const app = express();
-const server = require("http").createServer(app);
-const wss = new Websocket.Server({ server: server });
-const path = require("path");
-const hbs = require("express-handlebars");
-const sendData = require("./app/controllers/dataController");
+const express = require('express');
+const Websocket = require('ws');
+const server = require('http').createServer(app);
+const path = require('path');
+const hbs = require('express-handlebars');
 
-const routes = require("./routes");
+const routes = require('./routes');
+const sendData = require('./app/controllers/dataController');
+
+const wss = new Websocket.Server({ server: server });
+
+// data from file csv
+const fs = require('fs');
+const { parse } = require('csv-parse');
+var result = [];
 
 // config websocket
-wss.on("connection", (ws, req) => {
-  sendData(ws);
-  ws.send("Welcome new client");
-  ws.on("message", (message) => {
-    console.log(`received ${message}`);
-    ws.send("Got your message its" + message);
-  });
+wss.on('connection', (ws, req) => {
+    fs.createReadStream('./data_test.csv') // read data from file .csv
+        .pipe(parse({ delimiter: ',', from_line: 2 }))
+        .on('data', function (row) {
+            result.push(row);
+        })
+        .on('error', function (error) {
+            console.log(error.message);
+        })
+        .on('end', function () {
+            sendData(ws, result);
+        });
+
+    ws.on('message', (message) => {
+        console.log(`received ${message}`);
+        ws.send('Got your message its' + message);
+    });
 });
 
 // config static files
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 
 // config JSON
@@ -33,20 +49,20 @@ app.use(express.json());
 
 // config view engine
 app.engine(
-  "hbs",
-  hbs.engine({
-    extname: ".hbs",
-  })
+    'hbs',
+    hbs.engine({
+        extname: '.hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resource/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
 
 // config route
 routes(app);
 
 // start server
 server.listen(3000, () => {
-  console.log(`server is listening in port 3000`);
+    console.log(`server is listening in port 3000`);
 });
 
 module.exports = wss;
