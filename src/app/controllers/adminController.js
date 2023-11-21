89e223d08b53349a00a337275746b347a5584b34
nhanbui1512 <buithiennhan0345@gmail.com>
2023-11-21 23:04:49 +0700

    handle navigate login

--- src/app/controllers/adminController.js
+++ src/app/controllers/adminController.js
@@ -37,13 +37,42 @@ class AdminController {
   }
 
   async profile(req, res) {
-    return res.render('admin/profile.hbs', { layout: 'adminLayout.hbs' });
+    const id = req.session.userId;
+    const profileData = await User.findById(id);
+
+    return res.render('admin/profile.hbs', {
+      layout: 'adminLayout.hbs',
+      user: profileData.toObject(),
+    });
   }
 
-  async createAccount(req, res) {
+  async createAccountView(req, res) {
     return res.render('admin/createAccount.hbs', {
       layout: 'adminLayout.hbs',
     });
+  }
+
+  async registerAccount(req, res) {
+    const data = {
+      UserName: req.body.UserName,
+      Email: req.body.Email,
+      Password: req.body.Password,
+      PhoneNumber: req.body.PhoneNumber,
+      Address: req.body.Address,
+    };
+
+    try {
+      const isValidUser = mongooseToObject(await User.findOne({ Email: data.Email }));
+      if (isValidUser === null) {
+        await User.create(data);
+        return res.status(200).json({ result: true });
+      } else {
+        return res.status(200).json({ result: false });
+      }
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).json({ result: false, message: error.message });
+    }
   }
 
   async changePassWord(req, res) {
@@ -66,6 +95,52 @@ class AdminController {
       }
     } catch (error) {
       return res.status(200).json({ result: false });
+    }
+  }
+
+  async updateUser(req, res) {
+    const userId = req.query.ID;
+    const data = {
+      UserName: req.body.UserName,
+      Addresss: req.body.Address,
+      PhoneNumber: req.body.PhoneNumber,
+    };
+
+    console.log(data);
+    try {
+      const user = await User.findById(userId);
+      if (user.toObject() === null) {
+        throw new Error('not found user');
+      }
+
+      // user.UserName = data.UserName;
+      // user.Address = data.Addresss;
+      // user.PhoneNumber = data.PhoneNumber;
+
+      // await user.save();
+      return res.redirect('/admin/list-user');
+    } catch (error) {
+      console.log(error.message);
+      return res.render('error.hbs', { layout: false });
+    }
+  }
+
+  async updateProfile(req, res) {
+    const data = {
+      UserName: req.body.UserName,
+      PhoneNumber: req.body.PhoneNumber,
+      Address: req.body.Address,
+    };
+    try {
+      const user = await User.findById(req.session.userId);
+      user.UserName = data.UserName;
+      user.PhoneNumber = data.PhoneNumber;
+      user.Address = data.Address;
+      await user.save();
+      return res.redirect('/admin/profile');
+    } catch (error) {
+      console.log(error.message);
+      res.send(`Error ${error.message}`);
     }
   }
 }
